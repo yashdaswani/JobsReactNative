cfef2730c067537dd2654728f4287ce47649f350
yash daswani <yashdaswani2504@gmail.com>
2024-02-13 01:52:26 +0530

    update

---
+++ app/_layout.js
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+
+// SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "index",
+};
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack initialRouteName="index">
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
+
+export default Layout;
---
+++ hook/useFetch.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+// import { RAPID_API_KEY } from "@env";
+// const rapidKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: {
+      ...query,
+    },
+    headers: {
+      "X-RapidAPI-Key": "a441e081a0msh7bee1805bbc85c0p17f172jsne6026aa1d95f",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
